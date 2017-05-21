7331ca5dd9d3aa870ede931b99e1739a7c2f0d8b
Alec Johnson <alecjohnson55@gmail.com>
2017-05-21 16:51:11 -0700

    add isolate and right propagate

--- bit_tricks.py
+++ bit_tricks.py
@@ -19,3 +19,11 @@ def toggle_nth_bit(x, n):
 
 def turn_off_rightmost_one_bit(x):
     return x & (x - 1)
+
+def isolate_rightmost_one_bit(x):
+    # -x == ~(x + 1)
+    return x & ~(x + 1)
+
+def right_propagate_rightmost_set_bit(x):
+    # doesn't work on 0
+    return x | (x - 1)
