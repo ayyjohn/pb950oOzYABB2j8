292d593535179324e8d43e4e10ffc46fba46f8e7
Alec Johnson <alecjohnson55@gmail.com>
2017-05-24 14:08:03 -0700

    add O(n) solution

--- dutch_national_flag.py
+++ dutch_national_flag.py
@@ -37,3 +37,41 @@ def partition(array, i):
     return less + equal + greater
 
 print(partition([1, 5, 8, 3, 9, 0, 4, 3], 3))
+
+# this can be done without using O(n) extra memory though
+# by passing through the array twice, on the first pass
+# grouping all elements less than the pivot to the left
+# of the pivot, and then on the second pass grouping all
+# elements greater than the pivot to the right
+
+def partition(array, i):
+    less = 0
+    for j in range(len(array)):
+        if array[j] < array[i]:
+            array[less], array[j] = array[j], array[less]
+            less += 1
+    more = len(array) - 1
+    for j in reversed(range(len(array))):
+        if array[j] > array[i]:
+            array[more], array[j] = array[j], array[more]
+            more -= 1
+
+    return array
+
+
+print(partition([1, 5, 8, 3, 9, 0, 4, 3], 3))
+
+def partition(array, i):
+    pivot = array[i]
+    smaller, equal, larger = 0, 0, len(array)
+
+    while equal < larger:
+        if array[equal] < pivot:
+            array[smaller], array[equal] = array[equal], array[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif array[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            array[equal], array[larger] = array[larger], array[equal]
+    return array
