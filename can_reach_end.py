65c9e59658775cb47a606c07e9cd867e6b0f8aa7
Alec Johnson <alecjohnson55@gmail.com>
2017-05-25 22:42:15 -0700

    add iterative approach

--- can_reach_end.py
+++ can_reach_end.py
@@ -20,4 +20,16 @@ print(can_reach_end([0, 1]))
 print(can_reach_end([3, 3, 1, 0, 2, 0, 1]))
 print(can_reach_end([3, 2, 0, 0, 2, 0, 1]))
 
+# there's also an iterative approach. starting with the first
+# element, track the farthest element that we can reach. the farthest
+# element that we can reach is at i + array[i], and if at any time
+# i surpasses that value then we can't get to the end of the array
 
+def can_reach_end(array):
+    furthest_so_far, last_index = 0, len(array) - 1
+    i = 0
+
+    while i <= furthest_so_far and furthest_so_far < last_index:
+        furthest_so_far = max(furthest_so_far, array[i] + i)
+        i += 1
+    return furthest_so_far >= last_index
