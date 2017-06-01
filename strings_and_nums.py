5280f51a16c45f9c5b989a3bcc00139da353a1b0
Alec Johnson <alecjohnson55@gmail.com>
2017-05-31 21:33:47 -0700

    add optimized algorithm using multiplication

--- strings_and_nums.py
+++ strings_and_nums.py
@@ -32,6 +32,26 @@ print(string_to_num('-1000'))
 print(string_to_num('5000'))
 print(string_to_num('0'))
 
+# this brute force algorithm can be improved. iterate over the number
+# from the leftmost value and multiply a running partial value by 10
+# and add that digit. for example 314 can be made by partial = 3,
+# then partial = 3x10 + 1, then partial = 31x10 + 4.
+import functools
+import string
+
+def string_to_num(s):
+    return functools.reduce(lambda running_sum,
+                  c: running_sum * 10 + string.digits.index(c),
+                  s[s[0] == '-':],
+                  0) * (-1 if s[0] == '-' else 1)
+
+
+print(string_to_num('-1000'))
+print(string_to_num('5000'))
+print(string_to_num('0'))
+
+
+
 STRS = {
     0: '0',
     1: '1',
