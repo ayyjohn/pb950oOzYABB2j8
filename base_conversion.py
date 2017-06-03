87ee41c84c15a0850ea95a5ed56feebe101a3087
Alec Johnson <alecjohnson55@gmail.com>
2017-06-03 13:14:56 -0700

    remove print statements

--- base_conversion.py
+++ base_conversion.py
@@ -3,19 +3,20 @@
 # that number in base b2 as a string
 
 def base_convert(s, b1, b2):
+    if s == "0":
+        return 0
     x = -1
     val = int(s)
     output = ""
-    x = -1
     while 2**(x + 1) <= val:
         x += 1
     for i in reversed(range(x + 1)):
-        print(i)
         if val >= 2**i:
             output += '1'
             val -= 2**i
         else:
             output += '0'
     return output
-print(base_convert(15, 0, 0))
-print(base_convert(8, 0, 0))
+print(base_convert("15", 0, 0))
+print(base_convert("8", 0, 0))
+print(base_convert("0", 0, 0))
