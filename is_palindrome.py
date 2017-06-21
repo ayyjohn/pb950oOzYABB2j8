3b161f3d6c77e2c038f815b519e73cecf84f22e3
Alec Johnson <alecjohnson55@gmail.com>
2017-06-20 19:10:50 -0700

    add clearer case

--- is_palindrome.py
+++ is_palindrome.py
@@ -5,3 +5,15 @@
 def is_palindrome(string):
     # note that ~i = -i - 1
     return all(string[i] == string[~i] for i in range(len(string) // 2))
+
+def is_palindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i += 1
+        while not s[j].isalnum() and i < j:
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i, j = i+ 1, j - 1
+    return True
